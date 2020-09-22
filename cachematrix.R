538b718e60de3f9fe858048ba52f953c716db8ee
Arpit Singh <paularpit1993@gmail.com>
2020-09-23 02:41:12 +0530

    Week 3 Assignment by Arpit

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,17 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  j <- NULL     ## initialize j as NULL; will hold inverse matrix 
+  set <- function(y){  ## define the set function
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x  ## define the get function
+  setInverse <- function(inverse) j <<- inverse  ## assign value in parent environment
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("loading data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
+  }
